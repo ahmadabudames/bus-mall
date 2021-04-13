87df4eb65b0b94399fc81092c88d699aacaf1b6f
ahmadabudames <ahmadabudamesmais1999@gmail.com>
2021-04-13 15:26:38 +0300

    lab12

--- js/app.js
+++ js/app.js
@@ -19,12 +19,13 @@ let votesArray=[];
 let showArray=[];
 let nameArray=[];
 
+
 function busMall(name,source){
     this.name=name;
     this.source=source;
     this.votes=0;
     this.shown=0;
-
+nameArray.push(this.name);
     busMall.allBusMall.push(this)
 }
 
@@ -82,10 +83,14 @@ function renderImages(){
    
 
     leftElement.src=busMall.allBusMall[leftIndex].source;
+    busMall.allBusMall[leftIndex].shown++;
 
 rightElement.src=busMall.allBusMall[rightIndex].source;
+busMall.allBusMall[rightIndex].shown++;
 
 centerElement.src=busMall.allBusMall[centerIndex].source;
+busMall.allBusMall[centerIndex].shown++;
+
 }
 renderImages();
 
@@ -118,10 +123,14 @@ for(let i=0; i<busMall.allBusMall.length;i++){
 votesArray.push(busMall.allBusMall[i].votes);
 showArray.push(busMall.allBusMall[i].shown);}
 
+chart();
+
 leftElement.removeEventListener('click',userClick);
         centerElement.removeEventListener('click',userClick);
         rightElement.removeEventListener('click',userClick);
     }
+
+
 }
 function results(){
         let list=document.getElementById('result');
@@ -136,3 +145,43 @@ function results(){
         button.removeEventListener('click',results);
 
     }
+    function chart() {
+        let ctx = document.getElementById('ChartVeiw').getContext('2d');
+        
+        let chart= new Chart(ctx,{
+          // what type is the chart
+         type: 'bar',
+      
+        //  the data for showing
+         data:{
+          //  for the names
+            labels: nameArray,
+            
+            datasets: [
+              {
+              label: 'Goats votes',
+              data: votesArray,
+              backgroundColor: [
+                'Orange',
+              ],
+        
+              borderWidth: 1
+            },
+      
+            {
+              label: 'Goats shown',
+              data: showArray,
+              backgroundColor: [
+                'Gray',
+              ],
+        
+              borderWidth: 1
+            }
+            
+          ]
+          },
+          options: {}
+        });
+        
+      }
+  
